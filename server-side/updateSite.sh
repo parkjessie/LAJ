52973bc07f9242f58fb7c62193692d4e058de9ff
michaelScopic <michael@sanbornhome.com>
2022-04-06 12:01:18 -0700

    fixed a few files

--- server-side/updateSite.sh
+++ server-side/updateSite.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
-# !!! This script is to help automate updating our website !!!
+# --- !!! [This script is to help automate updating our website] !!! ---
 
-# --- Sourcing the variables from var.txt ---
-. ~/LAJ/server-side/var.txt 
+# --- [Sourcing the variables from var.txt] ---
+. ./.var.sh
 
-# --- Checking variables from var.txt ---
+# --- [Checking variables from var.txt] ---
 echo "
 $(tput setaf 1) !!! \$PROJECT_DIR is set as: $(tput sgr 0)$PROJECT_DIR 
 $(tput setaf 1) !!! \$PROJECT_SYSTEMD_FILE is set as: $(tput sgr 0)$PROJECT_SYSTEMD_FILE  $(tput sgr 0)"
@@ -20,39 +20,35 @@ else
   exit
 fi
 
-# --- Beginning the actual script ---
+# --- [Beginning the actual script] ---
 set $PREVIOUS_DIR=$(pwd)
 echo "$(tput setaf 3) --- Current dir is: $PREVIOUS_DIR --- $(tput sgr 0)"
 
-# --- Change directory to the project directory --- 
+# --- [Change directory to the project directory] --- 
 cd $PROJECT_DIR && echo "$(tput setaf 3) --- Changed dir to $PROJECT_DIR --- $(tput sgr 0)" 
 
-# -- 
 git pull  
 EXIT_CODE=$?
+# -- [Assigns "$EXIT_CODE" to the exit code of 'git pull'] ---
 
-# -- Assigns "$EXIT_CODE" to the exit code of the previous command 
-#  (which in this case is 'git pull') --
-
-# --- Checking status of 'git pull' and restarting the service file if necessary ---
+# --- [Checking status of 'git pull' and restarting the service file if necessary] ---
 if [ "`git pull`" == "Already up to date." ] 
 then
-  # -- If the result of 'git pull' outputs "Already up to date.", we will just 
-  # echo this text and exit the if-statement --
+  # -- [If the result of 'git pull' outputs "Already up to date.", we will just 
+  # echo this text and exit the if-statement] --
 
   echo "$(tput setaf 3) --- Directory is already up to date. No need to restart service --- $(tput sgr 0)"
 elif [ "$EXIT_CODE" = "0" ]
 then 
-  # -- If the exit code of 'git pull' is "0" 
+  # -- [If the exit code of 'git pull' is "0" 
   # (0 means the command was successful), we will restart our
-  # service with 'systemctl' --
+  # service with 'systemctl'] --
   echo "$(tput setaf 2) --- 'git pull' was successful. Trying to restart service file... --- $(tput sgr 0)"
   sudo systemctl restart $PROJECT_SYSTEMD_FILE
   if [ "$?" == "0" ]
   then 
-    # -- If the exit code of 'sudo systemctl restart $PROJECT_SYSTEMD_FILE'
-    # is 0, we will ask user if they want to check the 
-    # status of $PROJECT_SYSTEMD_FILE --
+    # -- [If the exit code of 'sudo systemctl restart $PROJECT_SYSTEMD_FILE'
+    # is 0, we will ask user if they want to check the status of $PROJECT_SYSTEMD_FILE] --
 
     read -p "$(tput setaf 1) --- Do you want to see the status of $PROJECT_SYSTEMD_FILE? This could be useful for debugging or making sure everything is all good. [Y/n] --- $(tput sgr 0) SYSTEMD_FILE_STATUS
     if [ "$SYSTEMD_FILE_STATUS" == "y" ] || [ "$SYSTEMD_FILE_STATUS" == " "] 
--- server-side/var.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set $PROJECT_DIR=/home/michael/LAJ/
-set $PROJECT_SYSTEMD_FILE=foo.service
