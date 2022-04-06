85bf971e0907511142f3e4add4024f975a0e9ac2
michaelScopic <michael@sanbornhome.com>
2022-04-06 11:59:22 -0700

    cleaned up code and corrected sourcing a file (still WIP)

--- server-side/setupScript.sh
+++ server-side/setupScript.sh
@@ -1,35 +1,43 @@
 #!/bin/bash 
 
+# --- !!! [This file is to setup the variables for 'updateSite.sh'] !!! ---
+
+# --- [Inital welcome text] ---
 echo "$(tput setaf 2) 
-##################################################
-#  Welcome to the setup script!                  #
-#  We need to set up a few directories in order  #
-#  for this to work properly.                    #
-# $(tput setaf 1) Also, please only run this script once. $(tput setaf 2)      # 
-##################################################
-"
-echo "
+######################################################
+#  Welcome to the setup script!                      #
+#  We need to set up a few directories in order for  #
+#  for $(tput setaf 3)'updateSite.sh'$(tput setaf 2) to work properly.             #
+######################################################
+
 ##############################################################
 #  Please enter the directory where the project is stored.   #
 #  Please use the full path (don't use '~' in the path).     #
-# $(tput sgr 0) (Correct: /home/ubuntu/MySite/) (Incorrect: ~/MySite) $(tput setaf 2)    #
+# $(tput sgr 0) (Correct: /home/ubuntu/MySite/)$(tput setaf 1) (Incorrect: ~/MySite) $(tput setaf 2)    #
 ############################################################## $(tput sgr 0)
 "
-
+# - [Asks user for the project dir] -
 while : ; do    # loops forever until user enters "y"  
     read -p  "Enter your project's directory here: " PROJECT_DIR
-    echo "$(tput setaf 3) You entered: $(tput sgr 0) $PROJECT_DIR"
+    echo "$(tput setaf 3)You entered: $(tput sgr 0)'$PROJECT_DIR'"
     read -p "Is this correct? [y/n]: " confirm
     [[ "$confirm" != "y" ]] || break    # breaks loop with "y" 
 done
 
-touch ~/LAJ/server-side/var.sh
-echo "#!/bin/bash" > ~/LAJ/server-side/var.sh
-echo "set \$PROJECT_DIR=$PROJECT_DIR" >> ~/LAJ/server-side/var.sh
+# - [Set up the file to store the variables] -
+touch .var.sh
+echo "#!/bin/bash" > .var.sh
+# - [End of setting up the variables file] -
 
+# - [Set up $PROJECT_DIR variable in '.var.sh'] -
+echo "set \$PROJECT_DIR = '$PROJECT_DIR'" >> .var.sh
+echo "$(tput setaf 4)--- Set \$PROJECT_DIR as '$PROJECT_DIR' in '.var.sh' ---$(tput sgr 0)"
+# - [End of setting up $PROJECT_DIR in '.var.sh] -
+
+# - [End of setting up project directory] -
 sleep 0.5
 
-# Systemd service file setup
+# --- [Systemd service file setup] ---
 echo "$(tput setaf 2) 
 #################################################
 #  Now please enter the systemd file            #
@@ -38,11 +46,13 @@ echo "$(tput setaf 2)
 "
 while : ; do 
     read -p  "Enter your project's systemd service file here: " PROJECT_SYSTEMD_FILE
-    echo "$(tput setaf 3) You entered: $(tput sgr 0) $PROJECT_SYSTEMD_FILE"
+    echo "$(tput setaf 3)You entered: $(tput sgr 0)'$PROJECT_SYSTEMD_FILE'"
     read -p "Is this correct? [y/n]: " confirm1
     [[ "$confirm1" != "y" ]] || break    
 done
 
-echo "set \$PROJECT_SYSTEMD_FILE=$PROJECT_SYSTEMD_FILE" >> ~/LAJ/server-side/var.sh
+echo "set \$PROJECT_SYSTEMD_FILE = '$PROJECT_SYSTEMD_FILE'" >> .var.sh 
+echo "$(tput setaf 4)--- Set \$PROJECT_SYSTEMD_FILE as '$PROJECT_SYSTEMD_FILE' in '.var.sh' ---$(tput sgr 0)"
+# --- [End of systemd service file setup] ---
 
 exit
