f899d8eec4a284515dfd72cb4f2289e70548d13f
parkjessie <jessiepark20040609@gmail.com>
2022-04-08 01:36:12 -0700

    CRUD and login in progress

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import render_template
+from __init__ import app
+from crudy.app_crud import app_crud
+from crudy.app_crud_api import app_crud_api
+
+app.register_blueprint(app_crud)
+app.register_blueprint(app_crud_api)
+
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+
+
+
+if __name__ == "__main__":
+    # runs the application on the repl development server
+    app.run(debug=True, port="5000")
